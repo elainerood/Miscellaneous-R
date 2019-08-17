aca5399377fdddfb32c7e394688a8b8ed6af647b
Elaine Rood <elaine.rood@gmail.com>
2019-08-17 17:01:29 -0400

    Initial commit for draw_cards
    Original version from 10/26/18

---
+++ draw_cards.R
@@ -0,0 +1,42 @@
+## Original version from 10/26/18
+
+library(dplyr)
+
+draw_cards <- function(n = 1,
+                       suits = c("Hearts", "Clubs", "Diamonds", "Spades"),
+                       per_suit = 13,
+                       repl = FALSE,
+                       use_special = TRUE,
+                       special_cards = c("1" = "Ace", "11" = "Jack",
+                                         "12" = "Queen", "13" = "King")) {
+  #' n = number of cards to draw
+  #' suits = names for the suits (vector of strings)
+  #' per_suit = number of cards per suit
+  #' repl = draw with/without replacement; default = without
+  #' use_special = use Ace, Jack, etc (vs 1, 11, etc)
+  #' special_cards = special designation for individual card values (e.g., ace, king, etc).
+  #'   Must be a named character vector where the names are the corresponding value in range 1:per_suit.
+
+  sc_vals <- as.numeric(names(special_cards))
+  card_vals <- as.character(1:per_suit)
+
+  if(use_special) {
+    for(i in seq_along(sc_vals)) {
+      x <- min(grep(pattern = paste0(sc_vals[i], "$"), x = card_vals))
+      card_vals[x] <- special_cards[i]
+    }
+  }
+
+  n_deck <- length(suits) * per_suit
+  deck <- data_frame(Value = rep(card_vals, times = length(suits)),
+                     Suit = rep(suits, each = per_suit))
+
+
+  draw_df <- slice(deck, sample(dim(deck)[1], n, replace = repl))
+  draw <- paste(draw_df$Value, "of", draw_df$Suit)
+  return(draw)
+}
+
+draw_cards(5)
+draw_cards(n = 3, suits = c("Rabbits", "Mice", "Hawks", "Foxes"),
+           per_suit = 5, use_special = FALSE)
